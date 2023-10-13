6a58490065cd8a9b18c9176b430e129ff90f2d01
Ahmed Mohamed <88363231+Ahmed-mohamed12@users.noreply.github.com>
2023-10-13 13:07:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let img=document.querySelector('.img')
+let container=document.querySelector('.container')
+function changephone(phone){
+    img.src=phone
+}
+function changebg(color){
+    container.style.background =color
+}
